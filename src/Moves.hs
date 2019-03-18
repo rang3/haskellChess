e5305081d2135090a186cfb8764df3e680c6456d
Russell Ang <russ103958@gmail.com>
2019-03-18 18:18:31 -0500

    implemented all piece moves

--- src/Moves.hs
+++ src/Moves.hs
@@ -33,36 +33,59 @@ strToCoordinate s = let (f, r) = C.strToPos s
 
 nextGameStates = undefined
 
-possibleMoves :: C.Piece -> Coordinate -> [Coordinate]
+possibleMoves :: C.Piece -> Coordinate -> [String]
 possibleMoves piece coordinate = case piece of 
-    C.Piece _   C.King -> possibleMovesForKing coordinate
-    C.Piece _   C.Queen -> possibleMovesForQueen coordinate
-    C.Piece _   C.Rook -> possibleMovesForRook coordinate
-    C.Piece _   C.Bishop -> possibleMovesForBishop coordinate
-    C.Piece _   C.Knight -> possibleMovesForKnight coordinate
-    C.Piece clr C.Pawn -> possibleMovesForPawn coordinate
+    C.Piece _       C.King -> possibleMovesForKing coordinate
+    C.Piece _       C.Queen -> possibleMovesForQueen coordinate
+    C.Piece _       C.Rook -> possibleMovesForRook coordinate
+    C.Piece _       C.Bishop -> possibleMovesForBishop coordinate
+    C.Piece _       C.Knight -> possibleMovesForKnight coordinate
+    C.Piece C.White C.Pawn -> possibleMovesForWhitePawn coordinate
+    C.Piece C.Black C.Pawn -> possibleMovesForBlackPawn coordinate
 
-possibleMovesForKing :: Coordinate -> [Coordinate]
-possibleMovesForKing (Coordinate f r) = tail
-    [Coordinate f' r' | f' <- [f,f+1,f-1], r' <- [r, r+1, r-1], validSquare f' r']
+possibleMovesForKing :: Coordinate -> [String]
+possibleMovesForKing c@(Coordinate f r) = tail
+    [show c ++ show (Coordinate f' r') 
+        | f' <- [f,f+1,f-1], r' <- [r, r+1, r-1], validSquare f' r']
 
-possibleMovesForQueen :: Coordinate -> [Coordinate]
+possibleMovesForQueen :: Coordinate -> [String]
 possibleMovesForQueen c1 = possibleMovesForRook c1 ++ possibleMovesForBishop c1
 
-possibleMovesForRook :: Coordinate -> [Coordinate]
-possibleMovesForRook (Coordinate f r ) = 
-    [Coordinate f r' | r' <- [0..7], r' /= r]
-    ++ [Coordinate f' r | f' <- [0..7], f' /= f]
+possibleMovesForRook :: Coordinate -> [String]
+possibleMovesForRook c@(Coordinate f r ) = 
+    [show c ++ show (Coordinate f r') | r' <- [0..7], r' /= r]
+    ++ [show c ++ show (Coordinate f' r) | f' <- [0..7], f' /= f]
 
-possibleMovesForBishop :: Coordinate -> [Coordinate]
-possibleMovesForBishop = undefined
+possibleMovesForBishop :: Coordinate -> [String]
+possibleMovesForBishop c@(Coordinate f r) = 
+    [show c ++ show (Coordinate (f+x) (r+x)) 
+        | x <- [-7.. -1]++[1..7], validSquare (f+x) (r+x)]
+    ++ [show c ++ show (Coordinate (f+x) (r-x)) 
+        | x <- [-7.. -1]++[1..7], validSquare (f+x) (r-x)]
 
-possibleMovesForKnight :: Coordinate -> [Coordinate]
-possibleMovesForKnight = undefined
+possibleMovesForKnight :: Coordinate -> [String]
+possibleMovesForKnight c@(Coordinate f r) = 
+    [show c ++ show (Coordinate f' r') 
+        | f' <- [f+1, f-1], r' <- [r+2, r-2], validSquare f' r']
+    ++ [show c ++ show (Coordinate f' r') 
+        | f' <- [f+2, f-2], r' <- [r+1, r-1], validSquare f' r']
 
 -- pawn promotion is not handled here
-possibleMovesForPawn :: Coordinate -> [Coordinate]
-possibleMovesForPawn = undefined
+possibleMovesForWhitePawn :: Coordinate -> [String]
+possibleMovesForWhitePawn c@(Coordinate f r) = case r of
+    1 -> (show c ++ show (Coordinate f (r+2))):movesNoJump -- starting jump
+    6 -> [m ++ p | m <- movesNoJump, p <- ["q","r","b","n"]] -- promotion
+    _ -> movesNoJump
+    where movesNoJump = [show c ++ show (Coordinate f' (r+1))
+                            | f' <- [f, f+1, f-1]]
+
+possibleMovesForBlackPawn :: Coordinate -> [String]
+possibleMovesForBlackPawn c@(Coordinate f r) = case r of
+    1 -> [m ++ p | m <- movesNoJump, p <- ["q","r","b","n"]] -- promotion
+    6 -> (show c ++ show (Coordinate f (r-2))):movesNoJump -- starting jump
+    _ -> movesNoJump
+    where movesNoJump = [show c ++ show (Coordinate f' (r-1))
+                            | f' <- [f, f+1, f-1]]
 
 validSquare :: Int -> Int -> Bool
 validSquare f r = f >= 0 && f < 8 && r >= 0 && r < 8
--- test/MovesSpec.hs
+++ test/MovesSpec.hs
@@ -13,136 +13,196 @@ spec = do
     describe "possibleMoves" $ do
         it "returns correct coordinates for king in lower left corner" $ do
             S.fromList (possibleMoves whiteKing (strToCoordinate "a1")) `shouldBe` 
-                ( S.fromList $ map (strToCoordinate) [ "a2", "b1", "b2"])
+                ( S.fromList [ "a1a2", "a1b1", "a1b2"])
         it "returns correct coordinates for king in upper right corner" $ do
             S.fromList (possibleMoves whiteKing (strToCoordinate "h8")) 
                 `shouldBe` 
-                    ( S.fromList $ map strToCoordinate 
-                        [ "g8"
-                        , "g7"
-                        , "h7"
+                    ( S.fromList 
+                        [ "h8g8"
+                        , "h8g7"
+                        , "h8h7"
                         ]
                     )
         it "returns correct coordinates for king in center" $ do
             S.fromList (possibleMoves whiteKing (strToCoordinate "d4")) 
                 `shouldBe` 
-                    ( S.fromList $ map strToCoordinate 
-                        [ "d3"
-                        , "c3"
-                        , "c4"
-                        , "c5"
-                        , "d5"
-                        , "e5"
-                        , "e4"
-                        , "e3"
+                    ( S.fromList 
+                        [ "d4d3"
+                        , "d4c3"
+                        , "d4c4"
+                        , "d4c5"
+                        , "d4d5"
+                        , "d4e5"
+                        , "d4e4"
+                        , "d4e3"
                         ]
                     )
         it "returns correct coordinates for Queen in center" $ do
             S.fromList (possibleMoves whiteQueen (strToCoordinate "d4"))
                 `shouldBe` 
-                    ( S.fromList $ map strToCoordinate
-                        [ "d1"
-                        , "d2"
-                        , "d3"
-                        , "d5"
-                        , "d6"
-                        , "d7"
-                        , "d8"
-                        , "a4"
-                        , "b4"
-                        , "c4"
-                        , "e4"
-                        , "f4"
-                        , "g4"
-                        , "h4"
-                        , "a1"
-                        , "b2"
-                        , "c3"
-                        , "e5"
-                        , "f6" 
-                        , "g7"
-                        , "h8"
-                        , "c5"
-                        , "b6"
-                        , "a7"
-                        , "e3"
-                        , "f2"
-                        , "g1"
+                    ( S.fromList
+                        [ "d4d1"
+                        , "d4d2"
+                        , "d4d3"
+                        , "d4d5"
+                        , "d4d6"
+                        , "d4d7"
+                        , "d4d8"
+                        , "d4a4"
+                        , "d4b4"
+                        , "d4c4"
+                        , "d4e4"
+                        , "d4f4"
+                        , "d4g4"
+                        , "d4h4"
+                        , "d4a1"
+                        , "d4b2"
+                        , "d4c3"
+                        , "d4e5"
+                        , "d4f6" 
+                        , "d4g7"
+                        , "d4h8"
+                        , "d4c5"
+                        , "d4b6"
+                        , "d4a7"
+                        , "d4e3"
+                        , "d4f2"
+                        , "d4g1"
                         ]
                     )
         it "returns correct coordinates for Rook in center" $ do
             S.fromList ( possibleMoves whiteRook (strToCoordinate "d4"))
                 `shouldBe` 
-                    ( S.fromList $ map strToCoordinate
-                        [ "d1"
-                        , "d2"
-                        , "d3"
-                        , "d5"
-                        , "d6"
-                        , "d7"
-                        , "d8"
-                        , "a4"
-                        , "b4"
-                        , "c4"
-                        , "e4"
-                        , "f4"
-                        , "g4"
-                        , "h4"
+                    ( S.fromList
+                        [ "d4d1"
+                        , "d4d2"
+                        , "d4d3"
+                        , "d4d5"
+                        , "d4d6"
+                        , "d4d7"
+                        , "d4d8"
+                        , "d4a4"
+                        , "d4b4"
+                        , "d4c4"
+                        , "d4e4"
+                        , "d4f4"
+                        , "d4g4"
+                        , "d4h4"
                         ]
                     )
         it "returns correct coordinates for bishop in center" $ do
             S.fromList (possibleMoves whiteBishop (strToCoordinate "d4"))
                 `shouldBe` 
-                    ( S.fromList $ map strToCoordinate
-                        [ "a1"
-                        , "b2"
-                        , "c3"
-                        , "e5"
-                        , "f6" 
-                        , "g7"
-                        , "h8"
-                        , "c5"
-                        , "b6"
-                        , "a7"
-                        , "e3"
-                        , "f2"
-                        , "g1"
+                    ( S.fromList 
+                        [ "d4a1"
+                        , "d4b2"
+                        , "d4c3"
+                        , "d4e5"
+                        , "d4f6" 
+                        , "d4g7"
+                        , "d4h8"
+                        , "d4c5"
+                        , "d4b6"
+                        , "d4a7"
+                        , "d4e3"
+                        , "d4f2"
+                        , "d4g1"
                         ]
                     )
         it "returns correct coordinates for knight in lower right corner" $ do
             S.fromList (possibleMoves whiteKnight (strToCoordinate "a1"))
                 `shouldBe` 
-                    ( S.fromList $ map strToCoordinate
-                        [ "b3"
-                        , "c2"
+                    ( S.fromList 
+                        [ "a1b3"
+                        , "a1c2"
                         ]
                     )
         it "returns correct coordinates for knight in center" $ do
             S.fromList (possibleMoves whiteKnight (strToCoordinate "d4"))
                 `shouldBe` 
-                    ( S.fromList $ map strToCoordinate
-                        [ "b3"
-                        , "c2"
-                        , "b5"
-                        , "c6"
-                        , "e6"
-                        , "f5"
-                        , "f3"
-                        , "e2"
+                    ( S.fromList 
+                        [ "d4b3"
+                        , "d4c2"
+                        , "d4b5"
+                        , "d4c6"
+                        , "d4e6"
+                        , "d4f5"
+                        , "d4f3"
+                        , "d4e2"
                         ]
                     )
-        it "returns correct coordinates for pawn in center" $ do
+        it "returns correct coordinates for white pawn in center" $ do
             S.fromList (possibleMoves whitePawn (strToCoordinate "d4"))
                 `shouldBe` 
-                    ( S.fromList $ map strToCoordinate
-                        [ "d5"
-                        , "d6"
-                        , "c5"
-                        , "e5"
-                        , "d3"
-                        , "d2"
-                        , "c3"
-                        , "e3"
+                    ( S.fromList 
+                        [ "d4d5"
+                        , "d4c5"
+                        , "d4e5"
+                        ]
+                    )
+        it "returns correct coordinates for white pawn in start position" $ do
+            S.fromList (possibleMoves whitePawn (strToCoordinate "d2"))
+                `shouldBe` 
+                    ( S.fromList 
+                        [ "d2d3"
+                        , "d2d4"
+                        , "d2c3"
+                        , "d2e3"
+                        ]
+                    )
+        it "returns correct coordinates for white pawn about to promote" $ do
+            S.fromList (possibleMoves whitePawn (strToCoordinate "d7"))
+                `shouldBe` 
+                    ( S.fromList 
+                        [ "d7d8q"
+                        , "d7d8r"
+                        , "d7d8b"
+                        , "d7d8n"
+                        , "d7e8q"
+                        , "d7e8r"
+                        , "d7e8b"
+                        , "d7e8n"
+                        , "d7c8q"
+                        , "d7c8r"
+                        , "d7c8b"
+                        , "d7c8n"
+                        ]
+                    )
+        it "returns correct coordinates for black pawn in center" $ do
+            S.fromList (possibleMoves blackPawn (strToCoordinate "d4"))
+                `shouldBe` 
+                    ( S.fromList 
+                        [ "d4d3"
+                        , "d4c3"
+                        , "d4e3"
+                        ]
+                    )
+        it "returns correct coordinates for black pawn in start position" $ do
+            S.fromList (possibleMoves blackPawn (strToCoordinate "d7"))
+                `shouldBe` 
+                    ( S.fromList 
+                        [ "d7d6"
+                        , "d7d5"
+                        , "d7c6"
+                        , "d7e6"
+                        ]
+                    )
+        it "returns correct coordinates for black pawn about to promote" $ do
+            S.fromList (possibleMoves blackPawn (strToCoordinate "d2"))
+                `shouldBe` 
+                    ( S.fromList 
+                        [ "d2d1q"
+                        , "d2d1r"
+                        , "d2d1b"
+                        , "d2d1n"
+                        , "d2e1q"
+                        , "d2e1r"
+                        , "d2e1b"
+                        , "d2e1n"
+                        , "d2c1q"
+                        , "d2c1r"
+                        , "d2c1b"
+                        , "d2c1n"
                         ]
                     )
 
@@ -163,3 +223,6 @@ whiteKnight = C.Piece C.White C.Knight
 
 whitePawn :: C.Piece
 whitePawn = C.Piece C.White C.Pawn
+
+blackPawn :: C.Piece
+blackPawn = C.Piece C.Black C.Pawn
